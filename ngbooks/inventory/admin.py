52b330a41ccce73b5b8768bd6dc3c80d90885c83
Olufunmilade Oshodi <olufunmilade.oshodi@andela.com>
2016-08-10 11:42:18 +0100

    Created models for the inventory app

--- ngbooks/inventory/admin.py
+++ ngbooks/inventory/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Book
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 1
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    inlines = [BookInline]
+
+
+admin.site.register(Category, CategoryAdmin)
--- ngbooks/inventory/models.py
+++ ngbooks/inventory/models.py
@@ -1,3 +1,29 @@
+"""
+Model for inventory app
+"""
+
 from django.db import models
 
 # Create your models here.
+
+
+class Base(models.Model):
+    # Base class for the inventory app models
+    name = models.CharField(max_length=200, unique=True)
+    description = models.CharField(max_length=200, blank=True, default='No description')
+
+    class Meta:
+        abstract = True
+
+    # returns a string representation of the object
+    def __str__(self):
+        return self.name
+
+
+class Category(Base):
+    pass
+
+
+class Book(Base):
+    book_author = models.CharField(max_length=200, default='Unknown')
+    book_cat = models.ForeignKey(Category, related_name='book')
